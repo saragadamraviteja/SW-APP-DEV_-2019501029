2844847a29659c6b2199697bf96f06d7d4ec44cc
saragadamraviteja <saragadamraviteja@gmai.com>
2020-04-15 18:56:02 +0530

    commit 1

---
+++ project1/code.py
@@ -0,0 +1,4 @@
+    <div class="form-group">
+      <label for="pwd">ReEnter Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="ReEnter password" name="pwd">
+    </div>
