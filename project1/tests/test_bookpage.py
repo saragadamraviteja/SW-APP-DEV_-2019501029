23d9262514f93ad271b3e47081ab8cc8a23dc282
Abhiram Rayala <55695264+sunbun2@users.noreply.github.com>
2020-04-25 22:17:50 +0530

    written some basic unit testing for the getbook  function created.

---
+++ project1/tests/test_bookpage.py
@@ -0,0 +1,18 @@
+import unittest
+from Testhelp import Testhelp
+from bookpage import *
+
+
+class Test_bookpage(Testhelp):
+
+    def test_valid(self):
+        book = getbook('081299289X')
+        self.assertEqual(book.title,'China Dolls')
+    
+    def test_invalid(self):
+        book = getbook('0345498127')
+        self.assertEqual(book.author,'David Nicholls')
+
+    def test_valid2(self):
+        book = getbook('0061053716')
+        self.assertEqual(book.year,'1991')
