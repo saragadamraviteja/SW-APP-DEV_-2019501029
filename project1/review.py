0d2744f0ed59c97975340861a55c84cddcd856f9
saragadamraviteja <saragadamraviteja@gmai.com>
2020-05-01 10:52:11 +0530

    completed password validation in register.html using java script

--- project1/review.py
+++ project1/review.py
@@ -1,6 +1,7 @@
 from models import Review
 
 def review_present(name,isbn):
+    print('nnan',name)
     if (Review.query.filter_by(username = name, isbn = isbn).first() == None):
-        return False
-    else: return True
+        return True
+    else: return False
